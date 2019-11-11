5eb4df392b1ad1a4ad9f13d9f9c097b004cce59a
davidchanho <davidho.crd@gmail.com>
2019-11-11 16:19:03 -0500

    add intial files

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burger_db;
+USE burger_dbl
+
+CREATE TABLE burger (
+    id INT NOT NULL AUTO_INCREMENT,
+    
+)
---
+++ keys.js
@@ -0,0 +1,4 @@
+exports.mysql = {
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+};
